a0f902e8dee92ceedaac46318998aee4c987fbd8
drishtiramesh <drishtiramesh1@gmail.com>
2020-06-12 14:43:20 +0530

    Update Resize_an_image_based_on_size_of_another.m

--- Resize_an_image_based_on_size_of_another.m
+++ Resize_an_image_based_on_size_of_another.m
@@ -1,6 +1,6 @@
-f=imread('C:\Users\Drishti\Desktop\matlabpics\veg.tiff');
-g=imread('C:\Users\Drishti\Desktop\matlabpics\cameraman.tiff');
+f=imread('veg.tiff');
+g=imread('cameraman.tiff');
 [r c b]=size(f);
 g=imresize(g,[r c]);
 
-imwrite(g,'C:\Users\Drishti\Desktop\matlabpics\resize.jpg');
+imwrite(g,'resize.jpg');
